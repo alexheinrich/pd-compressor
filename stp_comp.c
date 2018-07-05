d7e90eb80cdf92f632193751c3b9a3ed6cd6a7a4
Mert Aslanturk <mert.aslantuerk@holoplot.com>
2018-07-05 20:03:55 +0200

    stp comp commented

--- stp_comp.c
+++ stp_comp.c
@@ -26,24 +26,24 @@ void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, i
 {
 	float a_attack, a_release, in_abs, buffer, control_voltage;
 
-	// Set knee soft or hard
-	if (x->knee_width == 0.0f) { // hard knee
+	/** Set knee soft or hard*/
+	if (x->knee_width == 0.0f) { /** hard knee*/
 		x->knee_width = 0.0f;
 	}
-	else if (x->knee_width == 1.0f) { // soft knee
+	else if (x->knee_width == 1.0f) { /** soft knee*/
 		x->knee_width = 20.0f;
 	}
-	// Exception handling
+	/** Exception handling*/
 	else {
 		x->knee_width = 0.0f;
 	}
 
-	// Ratio exception handling
+	/** Ratio exception handling*/
 	if (x->ratio < 1.0f) {
 		x->ratio = 1.0f;
 	}
 
-	// Level detector exception handling
+	/** Level detector exception handling*/
 	if (x->attack <= 0.0f) {
 		x->attack = 0.001f;
 	}
@@ -52,19 +52,19 @@ void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, i
 		x->release = 0.001f;
 	}
 
-    // Calculate smooth detection filter coefficients eq. 7
+    /** Calculate smooth detection filter coefficients eq. 7*/
     a_attack = expf(-1.0f / (x->attack * sampling_rate));
     a_release = expf(-1.0f / (x->release * sampling_rate));
 
     float post_level_detector = 0.01f;
 
-    // Generate the control voltage
+    /** Generate the control voltage*/
     for (int i = 0; i < vector_size; ++i) {
     	in_abs = fabsf(in[i]);
-    	// Convert values in dB
+    	/** Convert values in dB*/
     	in_abs = 20.0f * logf(in_abs);
 
-    	// Gain computer
+    	/** Gain computer*/
     	if (2.0f * (in_abs - x->threshold) < -1.0f * x->knee_width) {
     		buffer = in_abs;
     	}
@@ -75,10 +75,10 @@ void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, i
     		buffer = x->threshold + (in_abs - x->threshold) / x->ratio;
     	}
 
-    	// Pre-level detector (feed forward loop)
+    	/** Pre-level detector (feed forward loop)*/
     	buffer = in_abs - buffer;
 
-    	// Level detector
+    	/** Level detector*/
     	if (buffer > post_level_detector) {
     		post_level_detector = a_attack * post_level_detector + (1.0f - a_attack) * buffer;
     	}
@@ -86,14 +86,11 @@ void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, i
     		post_level_detector = a_release * post_level_detector + (1.0f - a_release) * buffer;
     	}
 
-    	// Control voltage
+    	/** Control voltage*/
     	control_voltage = x->makeup_gain - post_level_detector;
 
-    	// Convert the control voltage to linear and apply it to the audio.
-<<<<<<< HEAD
-    	out[i] = in[i] * powf(10,control_voltage/20);
-=======
+    	/** Convert the control voltage to linear and apply it to the audio.*/
     	out[i] = in[i] * powf(10.0f, control_voltage / 20.0f);
->>>>>>> 823a1a0fbdf98b08251d4cf4fb1c094d970cff47
+
     }
 }
