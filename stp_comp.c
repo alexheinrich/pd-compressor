26c5c68f449ede59a161ec4f91fbac24b25fa8ab
alexheinrich <alex.heinrich@icloud.com>
2018-07-10 18:42:28 +0200

    Prevent log(0) and divide by 0 in knee.

--- stp_comp.c
+++ stp_comp.c
@@ -11,7 +11,7 @@ stp_comp *stp_comp_new()
     x->knee_width = 1.0f;
     x->attack = 0.01f;
     x->release = 0.02f;
-    x->post_level_detector = 0.01f;
+    x->post_level_detector = 0.0f;
 
     return x;
 }
@@ -25,18 +25,6 @@ void stp_comp_free(stp_comp *x)
 void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, int vector_size, float sampling_rate)
 {
 	float a_attack, a_release, in_abs, buffer, control_voltage;
-
-	// Set knee soft or hard
-	if (x->knee_width == 0.0f) { // hard knee
-		x->knee_width = 0.0f;
-	}
-	else if (x->knee_width == 1.0f) { // soft knee
-		x->knee_width = 20.0f;
-	}
-	// Exception handling
-	else {
-		x->knee_width = 0.0f;
-	}
 
 	// Ratio exception handling
 	if (x->ratio < 1.0f) {
@@ -56,13 +44,14 @@ void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, i
     a_attack = expf(-1.0f / (x->attack * sampling_rate));
     a_release = expf(-1.0f / (x->release * sampling_rate));
 
-    float post_level_detector = 0.01f;
-
     // Generate the control voltage
     for (int i = 0; i < vector_size; ++i) {
     	in_abs = fabsf(in[i]);
-    	// Convert values in dB
-    	in_abs = 20.0f * logf(in_abs);
+
+    	if (in_abs != 0.0f) {
+    		// Convert values in dB
+    		in_abs = 20.0f * logf(in_abs);
+    	}
 
     	// Gain computer
     	if (2.0f * (in_abs - x->threshold) < -1.0f * x->knee_width) {
@@ -79,15 +68,15 @@ void stp_comp_perform(stp_comp *x, STP_INPUTVECTOR *in, STP_OUTPUTVECTOR *out, i
     	buffer = in_abs - buffer;
 
     	// Level detector
-    	if (buffer > post_level_detector) {
-    		post_level_detector = a_attack * post_level_detector + (1.0f - a_attack) * buffer;
+    	if (buffer > x->post_level_detector) {
+    		x->post_level_detector = a_attack * x->post_level_detector + (1.0f - a_attack) * buffer;
     	}
     	else {
-    		post_level_detector = a_release * post_level_detector + (1.0f - a_release) * buffer;
+    		x->post_level_detector = a_release * x->post_level_detector + (1.0f - a_release) * buffer;
     	}
 
     	// Control voltage
-    	control_voltage = x->makeup_gain - post_level_detector;
+    	control_voltage = x->makeup_gain - x->post_level_detector;
 
     	// Convert the control voltage to linear and apply it to the audio.
     	out[i] = in[i] * powf(10.0f, control_voltage / 20.0f);
